6b865bd61ca86232faa2fb371120ee86986c596e
charlitodev <carenzdelarosa@gmail.com>
2024-06-18 08:53:40 +0800

    test: added jest testing purposes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import LandingPage from '@/modules/landing/LandingPage';
 export default function Home() {
   return (
     <main>
+      <h1 className="hidden">__test__</h1>
       <LandingPage />
     </main>
   );
