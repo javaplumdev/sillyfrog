45519796226ac03826bde40874fcce0dd0e84877
charlitodev <carenzdelarosa@gmail.com>
2024-06-19 13:26:41 +0800

    feat: added static hamburger icon at nav for smaller devices

--- src/modules/landing/LandingNav.tsx
+++ src/modules/landing/LandingNav.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import Link from 'next/link';
-import { buttonVariants } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
+import { Menu } from 'lucide-react';
+import { buttonVariants } from '@/components/ui/button';
 
 const LandingNav = () => {
   return (
-    <div className="p-2 sm:p-3 m-4 sm:my-8 mx-0 px-6 sm:px-12 bg-primary text-white rounded-xl flex justify-between items-center">
+    <div className="p-2 sm:p-3 m-4 sm:my-8 mx-0 px-6 sm:px-12 rounded-xl flex justify-between items-center text-white bg-primary bg-opacity-50 backdrop-blur-md shadow-lg">
       <h2 className="font-bold">&#x1F438; Sillyfrog</h2>
       <div>
-        <li className="list-none">
+        <li className="list-none hidden sm:block">
           <ul>
             <Link href="/about" className={buttonVariants({ variant: 'ghost' })}>
               About us
@@ -21,6 +22,10 @@ const LandingNav = () => {
             </Link>
           </ul>
         </li>
+      </div>
+
+      <div className="sm:hidden">
+        <Menu size={30} />
       </div>
     </div>
   );
