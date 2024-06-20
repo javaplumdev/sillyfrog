7a70bfc49d9a5a5b88be07f0fc564a9a7ea44963
charlitodev <carenzdelarosa@gmail.com>
2024-06-20 13:44:10 +0800

    fix: adjusted 404 error page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ import BaseButton from '@/components/base/buttons/BaseButton';
 import { ShieldX } from 'lucide-react';
 import { cn } from '@/lib/utils';
 
-const headerOneCn = { cn: 'mt-8 text-3xl md:text-5xl' };
+const headerOneCn = { cn: 'mt-8 text-3xl md:text-6xl' };
 
 const NotFound = () => {
   return (
@@ -13,7 +13,7 @@ const NotFound = () => {
           {/* <RiAlarmWarningFill size={60} className="drop-shadow-glow animate-flicker text-red-500" /> */}
           <div className="flex items-center">
             <ShieldX className="text-red-500 mt-6 mr-3" size={40} />
-            <h1 className={cn(headerOneCn.cn, 'text-red-500')}>404</h1>
+            <h1 className={cn(headerOneCn.cn, 'text-red-500 font-extrabold')}>404</h1>
           </div>
           <h1 className={cn(headerOneCn.cn)}>Page Not Found</h1>
           <p className="my-6">Sorry, the page you are looking for could not be found.</p>
