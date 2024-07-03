fadb43c7859ed6478ee9a8e7ad06c1412598f1b4
charlitodev <carenzdelarosa@gmail.com>
2024-07-03 09:22:38 +0800

    fix: updated footer

--- src/modules/landing/LandingFooter.tsx
+++ src/modules/landing/LandingFooter.tsx
@@ -4,13 +4,17 @@ import Link from 'next/link';
 const LandingFooter = () => {
   return (
     <div className="mt-64 mb-12">
-      <div className="bottom-0 bg-gray-300 p-3 text-center w-full rounded-xl">
-        <span className="text-gray-800">
+      <div className="bottom-0 p-3 text-center w-full rounded-xl">
+        <small className="text-gray-800 text-sm">
           Icons by{' '}
           <Link href="https://icons8.com/" target="_blank" className="underline">
             Icons8
           </Link>
-        </span>
+        </small>
+
+        <br />
+
+        <small className="text-gray-800 text-sm">Sillyfrog all rights reserved</small>
       </div>
     </div>
   );
