c01b87ed1b80fd57a2985df9d7a4442ac90d12e4
charlitodev <carenzdelarosa@gmail.com>
2024-07-03 13:17:05 +0800

    feat: init password checker

--- src/modules/app/PasswordCheckerProvider.tsx
+++ src/modules/app/PasswordCheckerProvider.tsx
@@ -1,20 +1,49 @@
 import React, { createContext } from 'react';
+import { useForm } from 'react-hook-form';
 
 interface PasswordCheckerProps {
-  testValue: string;
+  passwordReq: Array<PasswordReqProps>;
 }
 
+interface PasswordReqProps {
+  text: string;
+  match: boolean;
+}
+
+export type PasswordReqArrayProps = PasswordReqProps[];
+
 const obj: PasswordCheckerProps = {
-  testValue: '',
+  passwordReq: [],
 };
 
 export const PasswordCheckerContext = createContext(obj);
 
 const PasswordCheckerProvider = ({ children }: any) => {
-  let testValue: string = '';
+  const { watch } = useForm();
+
+  const password: string = watch?.('password') || '';
+
+  const passwordReq: PasswordReqArrayProps = [
+    {
+      text: 'Must be at least 8 characters',
+      match: (password || []).length >= 8,
+    },
+    {
+      text: 'Must contain at least 1 uppercase letter.',
+      match: /[A-Z]/.test(password),
+    },
+    {
+      text: 'Must contain at least 1 lowercase letter',
+      match: /[a-z]/.test(password),
+    },
+    {
+      text: 'Must contain at least 1 number',
+      match: /\d/.test(password),
+    },
+  ];
 
   return (
-    <PasswordCheckerContext.Provider value={{ testValue }}>
+    <PasswordCheckerContext.Provider value={{ passwordReq }}>
       {children}
     </PasswordCheckerContext.Provider>
   );
